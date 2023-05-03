7ef05ca5153795347267436ae89c2c1b63fc19a5
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-05-03 14:51:29 -0400

    hopefully something good

---
+++ LobbyistRehaulwithReact/ClientApp/src/Pages/Homepage.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+
+function Homepage() {
+    return (
+        <>
+
+
+        </>
+    );
+}
--- LobbyistRehaulwithReact/ClientApp/src/index.js
+++ LobbyistRehaulwithReact/ClientApp/src/index.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './styles/LoginPageStyle/Background.css';
 import React, { Component } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import Login from './components/Login';
 import Banner from './components/Banner';
 
@@ -9,8 +10,10 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-    <>
-        <Banner />
-        <Login />
-    </>
+    <BrowserRouter>
+        <>
+            <Banner />
+            <Login />
+        </>
+    </BrowserRouter>
 );
