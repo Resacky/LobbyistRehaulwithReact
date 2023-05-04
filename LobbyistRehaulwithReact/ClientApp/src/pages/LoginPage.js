bd72cab58866b47a9878dc121afaf58afd1f5004
Resacky <Ruiz-joseph@hotmail.com>
2023-05-04 10:04:43 -0400

    Made changes

--- LobbyistRehaulwithReact/ClientApp/src/pages/LoginPage.js
+++ LobbyistRehaulwithReact/ClientApp/src/pages/LoginPage.js
@@ -1,8 +1,8 @@
 ﻿import React from 'react';
 import '../styles/LoginPageStyle/Background.css';
 
-import Login from '../components/login';
-import Banner from '../components/banner';
+import Login from '../components/Login';
+import Banner from '../components/Banner';
 
 function LoginPage() {
     return (
